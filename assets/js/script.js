163ffd908d33b68bf9949ef577443a7a2fdde4f5
Gaetanerk <gaetan.turben@outlook.com>
2022-04-25 15:40:54 +0200

    correction exo 4.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
-let sex = document.querySelector("#sex");
-let age = document.querySelector("#age");
-let checkBtn = document.querySelector("#checkBtn");
+const sex = document.querySelector("#sex");
+const age = document.querySelector("#age");
+const checkBtn = document.querySelector("#checkBtn");
+
+// checkBtn.addEventListener("click", () => {
+//   if (sex.value === "homme" && age.value > 20) {
+//     alert("Vous payez l'impôt");
+//   } else if (sex.value === "femme" && age.value >= 18 && age.value <= 35) {
+//     alert("Vous payez l'impôt");
+//   } else {
+//     alert("Vous ne payez pas d'impôts");
+//   }
+// });
 
 checkBtn.addEventListener("click", () => {
-  if (sex.value === "homme" && age.value > 20) {
-    alert("Vous payez l'impôt");
-  } else if (sex.value === "femme" && age.value >= 18 && age.value <= 35) {
-    alert("Vous payez l'impôt");
+  if (
+    (age.value >= 20 && sex.value === "homme") ||
+    (age.value >= 18 && age.value <= 35 && sex.value === "femme")
+  ) {
+    result.textContent = "Vous payez l'impôt";
   } else {
-    alert("Vous ne payez pas d'impôts");
+    result.textContent = "Vous ne payez pas l'impôt";
   }
 });
